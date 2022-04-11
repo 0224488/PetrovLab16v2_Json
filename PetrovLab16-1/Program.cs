52b86c2ff1e61c091443f490c1872f2fc13d46eb
0224488 <0224488@inbox.ru>
2022-04-11 10:33:20 +0300

    Добавьте файлы проекта.

---
+++ PetrovLab16-1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+
+namespace PetrovLab16_1
+{
+	class Program
+	{
+		// Необходимо разработать программу для записи информации о товаре в текстовый файл в формате json.
+		// Разработать класс для моделирования объекта «Товар». 
+		// Предусмотреть члены класса «Код товара» (целое число), «Название товара» (строка), «Цена товара» (вещественное число).
+		// Создать массив из 5-ти товаров, значения должны вводиться пользователем с клавиатуры.
+		// Сериализовать массив в json-строку, сохранить ее программно в файл «Products.json».
+
+        static void Main(string[] args)
+		{
+			var products = new Product[5]; // Количество товаров
+			for (int i = 0; i < products.Length; i++)
+			{
+				products[i] = new Product();
+			}
+
+			// Вводим значения. Проверку вводимых значений не делаю.
+			for (int i = 0; i < products.Length; i++)
+			{
+				Console.Write($"Введите код товара {i + 1}: ");
+				products[i].Code = int.Parse(Console.ReadLine());
+
+				Console.Write($"Введите название товара {i + 1}: ");
+				products[i].Name = Console.ReadLine();
+
+				Console.Write($"Введите цену товара {i + 1}: ");
+				products[i].Price = double.Parse(Console.ReadLine());
+			}
+
+			// Сериализация в строку, используя System.Text.Json
+			var options = new JsonSerializerOptions
+			{
+				Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+				WriteIndented = true
+			};
+			var json = System.Text.Json.JsonSerializer.Serialize(products, options);
+
+            try
+            {
+				Console.WriteLine("Введите путь сохранения файла");
+				var path = Console.ReadLine() + "/Products.json";
+				using (var file = new StreamWriter(path))
+				{
+					file.WriteLine(json);
+					Console.WriteLine("Файл успешно сохранен");
+				}
+
+			}
+			catch
+            {
+				Console.WriteLine("Ошибка доступа или неверный путь. Файл не сохранен");
+			}
+			Console.ReadKey();
+		}
+
+		public class Product
+		{
+			// Код товара
+			public int Code { get; set; }
+
+			// Название товара
+			public string Name { get; set; }
+
+			// Цена товара
+			public double Price { get; set; }
+		}
+	}
+}
---
+++ PetrovLab16-2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+
+namespace PetrovLab16_2
+{
+	// Необходимо разработать программу для получения информации о товаре из json-файла.
+	// Десериализовать файл «Products.json» из задачи 1. Определить название самого дорогого товара.
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            try // Для проверки правильности пути и наличия файла ограничился исключением...
+            {
+				// Чтение строки из файла
+				Console.WriteLine("Введите путь к сохраненному файлу Products.json");
+
+				var path = Console.ReadLine() + "/Products.json";
+
+				using (StreamReader reader = new StreamReader(path))
+				{
+					string text = reader.ReadToEnd();
+
+					// Десериализация с помощью библиотеки System.Text.Json
+					var options = new JsonSerializerOptions
+					{
+						Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+						WriteIndented = true
+					};
+					var products = System.Text.Json.JsonSerializer.Deserialize(text, typeof(Product[]), options) as Product[];
+
+					string name = products[0].Name;
+					double maxPrice = products[0].Price;
+					for (int i = 1; i < products.Length; i++)
+					{
+						if (products[i].Price > maxPrice)
+						{
+							maxPrice = products[i].Price;
+							name = products[i].Name;
+						}
+					}
+					Console.WriteLine($"Самый дорогой товар: {name}");
+				}
+
+			}
+			catch
+            {
+				Console.WriteLine("Ошибка доступа или отсутствует файл по указанному пути");
+			}
+			Console.ReadKey();
+		}
+
+		public class Product
+		{
+			// Код товара
+			public int Code { get; set; }
+
+			// Название товара
+			public string Name { get; set; }
+
+			// Цена товара
+			public double Price { get; set; }
+		}
+	}
+}
